27e440c6dd2026f9184e04f134bf1507d0a3da15
Puneet Pathak <pp4july2006@gmail.com>
2026-01-05 08:37:00 +0000

    update the scholarships deadline and link the approroprita wesites

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,4 @@
+
 import type { User, Scholarship, Tip, Notification } from './types';
 
 export const user: User = {
@@ -14,33 +15,33 @@ export const scholarships: Scholarship[] = [
         name: 'Post Matric Scholarship for SC Students',
         provider: 'Ministry of Social Justice & Empowerment',
         amount: 'Upto ₹13,500 p.a.',
-        deadline: '2024-08-31',
+        deadline: '2024-12-31',
         eligibility: {
             state: ['All India'],
             category: ['SC'],
             income: '₹2.5 Lakh p.a.'
         },
-        link: 'https://scholarships.gov.in/'
+        link: 'https://scholarships.gov.in/fresh/newstdRegfrmInstruction'
     },
     {
         id: 's2',
         name: 'Merit Cum Means Scholarship for Professional and Technical Courses',
         provider: 'Ministry of Minority Affairs',
         amount: '₹20,000 p.a. + Fees',
-        deadline: '2024-09-30',
+        deadline: '2024-11-30',
         eligibility: {
             state: ['All India'],
             category: ['Minority'],
             income: '₹2.5 Lakh p.a.'
         },
-        link: 'https://scholarships.gov.in/'
+        link: 'https://scholarships.gov.in/fresh/newstdRegfrmInstruction'
     },
     {
         id: 's3',
         name: 'Mukhyamantri Medhavi Vidyarthi Yojana (MMVY)',
         provider: 'Government of Madhya Pradesh',
         amount: 'Full Course Fee',
-        deadline: '2024-08-15',
+        deadline: '2025-01-15',
         eligibility: {
             state: ['Madhya Pradesh'],
             category: ['General', 'OBC', 'SC', 'ST'],
@@ -52,19 +53,19 @@ export const scholarships: Scholarship[] = [
         name: 'Pragati Scholarship for Girl Students',
         provider: 'AICTE',
         amount: '₹50,000 p.a.',
-        deadline: '2024-10-31',
+        deadline: '2024-11-30',
         eligibility: {
             state: ['All India'],
             category: ['Girl Students'],
         },
-        link: 'https://www.aicte-pragati-saksham-gov.in/'
+        link: 'https://www.aicte-india.org/schemes/students-development-schemes/Pragati-Saksham-Scholarship-Scheme'
     },
     {
         id: 's5',
         name: 'Chief Minister\'s Scholarship Scheme',
         provider: 'Government of Haryana',
         amount: 'Upto ₹12,000 p.a.',
-        deadline: '2024-09-15',
+        deadline: '2024-12-20',
         eligibility: {
             state: ['Haryana'],
             category: ['General', 'SC', 'OBC'],
